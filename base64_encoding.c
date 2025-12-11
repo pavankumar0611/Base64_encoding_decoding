34cd2ba3b639882cd44125f54df59b3620fb85ec
pavankumar0611 <pavankohli0644@gmail.com>
2025-12-10 18:49:42 -0800

    encoding to base64 format with padding taking arguments via cmd line.

--- base64_encoding.c
+++ base64_encoding.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 char base64_table[]  = 
 { 'A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G', 'H' , 'I',
@@ -14,20 +15,14 @@ char base64_table[]  =
 
 	'+' , '-' };
 
-
-int main()
+void function_for_exact_3byte_encoding( char  *arr)
 {
-	char *arr;
-
-	printf("enter the string in multiple of 3's \n");
-	scanf("%s",arr);
-
 	unsigned char current_bit = 0 , next_bit = 0, save = 2;
-	for(int i = 0 ; i < 6;  i++)
+	for(int i = 0 ;i < 3;  i++)
 	{
 		if(save == 2) {
 			current_bit = arr[i] >> 2;
-			printf("%c ", base64_table[current_bit]);
+			printf("%c", base64_table[current_bit]);
 			next_bit = (arr[i] & 0x3);
 			save = save + 2;
 		}
@@ -36,7 +31,7 @@ int main()
 			current_bit = (current_bit >> 4);
 			next_bit = next_bit << 4;
 			current_bit = (next_bit  | current_bit);
-			printf("%c ", base64_table[current_bit]);
+			printf("%c", base64_table[current_bit]);
 			next_bit = arr[i] & 0xf;
 			save = save + 2;
 		}
@@ -44,8 +39,64 @@ int main()
 			next_bit  = next_bit << 2;
 			current_bit = ((arr[i] & 0xc0) >> 6);
 			current_bit = next_bit | current_bit;
-			printf("%c %c ",base64_table[current_bit], base64_table[arr[i] & 0x3f]);
+			printf("%c%c",base64_table[current_bit], base64_table[arr[i] & 0x3f]);
 			save = 2;
 		}
 	}
+}
+
+void function_for_byte_encoding(char *arr , int size)
+{
+	if (size % 2 == 0)
+	{
+		unsigned char current_bit = 0, next_bit = 0, save = 2;
+		for(int i = 0; i < size ; i++) {
+			if(save == 2) {
+				current_bit = arr[i] >> 2;
+				printf("%c",base64_table[current_bit]);
+				next_bit = arr[i] & 0x3;
+				save = save + 2;
+			}else if (save == 4) {
+				current_bit  = arr[i] & 0xf0;
+				current_bit = current_bit >> 4;
+				next_bit = next_bit << 4;
+				current_bit = (next_bit | current_bit);
+				printf("%c",base64_table[current_bit]);
+				next_bit = arr[i] & 0xf;
+				printf("%c=",base64_table[next_bit << 2]);
+			}
+		}
+	}
+	else {
+		unsigned char current_bit = 0, next_bit = 0;
+		current_bit = arr[0] >> 2;
+		printf("%c",base64_table[current_bit]);
+		next_bit =  arr[0] & 0x3;
+		printf("%c==",base64_table[next_bit << 4]);
+	}
+}
+
+int main(int argc , char* argv[])
+{
+	if ( argc < 2 ){
+		printf(" ./a.out < string> \n");
+		exit(1);
+	}
+
+	int length = strlen(argv[1]);
+	char *arr = argv[1];
+
+	for(int i = 0 ; i >= 0 ;i = i + 3)
+	{
+		if (length >= 3 ) {
+			function_for_exact_3byte_encoding(&arr[i]);
+			length = length - 3;
+		}
+		else {
+			function_for_byte_encoding(&arr[i], length);
+			length = 0;
+			break;
+		}
+	}
+	printf("\n");
 }
